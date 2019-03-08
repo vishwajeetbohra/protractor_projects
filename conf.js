8942ddada7714ed83abc226199c6fd4c7a8f0449
Vishwajeet Bohra <bohra.vishwajeet@gmail.com>
2019-03-09 00:25:56 +0530

    changes2

--- conf.js
+++ conf.js
@@ -1,5 +1,8 @@
 exports.config = {
+
     // launch1 locally when fields directConnect and seleniumAddress are not provided
+    // launch2 locally when fields directConnect and seleniumAddress are not provided
+
     chromeDriver: '/usr/local/lib/node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.40',
     seleniumServerJar: '/usr/local/lib/node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.13.0.jar',
     specs: ['D:\\Protractor\\JSFilesLocation\\test.js'],
